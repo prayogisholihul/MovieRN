cf7362459214f3f558d33a5b41556fdb696d7e4e
prayogisholihul <prayogisholihul@gmail.com>
2025-09-01 22:55:51 +0700

    Initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>profile</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
